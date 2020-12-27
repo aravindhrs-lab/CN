400233ce426fa3a0289077986229011581b230ca
Aravindh RS <aravindh.ravishankar@gmail.com>
2020-12-27 21:00:14 +0530

    Update

--- Lab8/dvr.py
+++ Lab8/dvr.py
@@ -1,44 +1,52 @@
-class Network():
-    def __init__(self, N):
-        self.matrix = []
-        self.N = N
+class Network:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = []
 
-    def addlink(self, u, v, w):
-        self.matrix.append((u, v, w))
+    def add_edge(self, s, d, w):
+        self.graph.append([s, d, w])
 
-    def table(self, dist, src):
-        print(f"Vector Table of {chr(ord('A')+src)}")
-        print("Dest\tCost")
-        for i in range(self.N):
-            print(f"{chr(ord('A')+i)}\t{dist[i]}")
+    def print_solution(self, dist, src, next_hop):
+        print("Routing table for ", src)
+        print("Dest \t Cost \t Next Hop")
+        for i in range(self.V):
+            print("{0} \t {1} \t {2}".format(i, dist[i], next_hop[i]))
 
-    def algorithm(self, src):
-        dist = [99] * self.N
+    def bellman_ford(self, src):
+
+        dist = [99] * self.V
         dist[src] = 0
+        next_hop = {src: src}
+        for _ in range(self.V - 1):
+            for s, d, w in self.graph:
+                if dist[s] != 99 and dist[s] + w < dist[d]:
+                    dist[d] = dist[s] + w
+                    if s == src:
+                        next_hop[d] = d
+                    elif s in next_hop:
+                        next_hop[d] = next_hop[s]
 
-        for _ in range(self.N - 1):
-            for u, v, w in self.matrix:
-                if dist[u] != 99 and dist[u] + w < dist[v]:
-                    dist[v] = dist[u] + w
+        for s, d, w in self.graph:
+            if dist[s] != 99 and dist[s] + w < dist[d]:
+                print("Network contains negative weight cycle")
+                return
 
-        self.table(dist, src)
+        self.print_solution(dist, src, next_hop)
 
 
 if __name__ == "__main__":
-
     matrix = []
-    n = int(input("Enter Number of Nodes : "))
-    print("Enter the Adjacency Matrix : ")
-
-    for _ in range(n):
-        m = list(map(int, input().split(" ")))
-        matrix.append(m)
+    print("Enter the no. of routers:")
+    n = int(input())
+    print("Enter the adjacency matrix : (Enter 99 for infinity)")
+    for i in range(0, n):
+        a = list(map(int, input().split(" ")))
+        matrix.append(a)
 
     g = Network(n)
-    for i in range(n):
-        for j in range(n):
-            if matrix[i][j] == 1:
-                g.addlink(i, j, 1)
+    for i in range(0, n):
+        for j in range(0, n):
+            g.add_edge(i, j, matrix[i][j])
 
-    for _ in range(n):
-        g.algorithm(_)
+    for k in range(0, n):
+        g.bellman_ford(k)
